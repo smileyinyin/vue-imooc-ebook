53ad81e37057b9eb5581fb974ab48d87734dec24
smile-yy <smile-y520@163.com>
2019-04-16 15:52:06 +0800

    开发资源

---
+++ 开发资源/第四章资源/4-7 localStorage/utils/localStorage.js
@@ -0,0 +1,45 @@
+import Storage from 'web-storage-cache'
+
+const localStorage = new Storage()
+
+export function setLocalStorage(key, value) {
+  return localStorage.set(key, value)
+}
+
+export function getLocalStorage(key) {
+  return localStorage.get(key)
+}
+
+export function removeLocalStorage(key) {
+  return localStorage.delete(key)
+}
+
+export function clearLocalStorage() {
+  return localStorage.clear()
+}
+
+export function setBookObject(fileName, key, value) {
+  let book = getLocalStorage(`${fileName}-info`)
+  if (!book) {
+    book = {}
+  }
+  book[key] = value
+  setLocalStorage(`${fileName}-info`, book)
+}
+
+export function getBookObject(fileName, key) {
+  let book = getLocalStorage(`${fileName}-info`)
+  if (book) {
+    return book[key]
+  } else {
+    return null
+  }
+}
+
+export function getFontFamily(fileName) {
+  return getBookObject(fileName, 'fontFamily')
+}
+
+export function saveFontFamily(fileName, font) {
+  return setBookObject(fileName, 'fontFamily', font)
+}
